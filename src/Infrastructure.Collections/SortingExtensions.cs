a62dd1a2c196cf86fe0f11a5f52b5f1d0b680e04
Nitin Issac Joy <nitin.i.joy@gmail.com>
2026-08-20 17:59:01 +0530

    preserve declaration order for equal-priority items.

--- src/Infrastructure.Collections/SortingExtensions.cs
+++ src/Infrastructure.Collections/SortingExtensions.cs
@@ -8,11 +8,6 @@ namespace Infrastructure.Collections
     {
         public static List<T> ByPriority<T>(
             this IEnumerable<T> src,
-            Func<T, int> key)
-        {
-            var list = src.ToList();
-            list.Sort((a, b) => key(a).CompareTo(key(b)));
-            return list;
-        }
+            Func<T, int> key) => src.OrderBy(key).ToList();
     }
 }
